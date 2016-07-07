ff6c81f3da6a59f54524f752b978790ee69b31a8
exceptionnotfound <mpjones428@gmail.com>
2016-07-07 12:44:46 -0700

    Added User DTO and API controller.

---
+++ WebApiArchitectureDemo.Api/Controllers/UsersController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiArchitectureDemo.Common.DTOs;
+
+namespace WebApiArchitectureDemo.Api.Controllers
+{
+    [RoutePrefix("users")]
+    public class UsersController : ApiController
+    {
+        [HttpGet]
+        [Route("all")]
+        public IHttpActionResult GetAll()
+        {
+            var users = new List<User>();
+
+            users.Add(new User()
+            {
+                FirstName = "Marco",
+                LastName = "Polo",
+                DateOfBirth = new DateTime(1254, 1, 14),
+                Username = "greatadventurer"
+            });
+
+            users.Add(new User()
+            {
+                FirstName = "Kublai",
+                LastName = "Khan",
+                DateOfBirth = new DateTime(1215, 9, 23),
+                Username = "khanofkhans"
+            });
+
+            users.Add(new User()
+            {
+                FirstName = "Kokachin",
+                LastName = "",
+                DateOfBirth = new DateTime(1255, 8, 17),
+                Username = "blueprincess"
+            });
+
+            return Ok(users);
+        }
+    }
+}
